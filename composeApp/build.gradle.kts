58cb4c83e7b0af8ec337957f4cc3dc8dac60fe92
JihyeHan <jihyeh919@gmail.com>
2025-12-29 17:57:02 +0900

    iOS ktor-client-darwin 의존성 추가

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -36,6 +36,9 @@ kotlin {
             implementation(libs.koin.android)
             implementation(libs.ktor.client.okhttp)
         }
+        iosMain.dependencies {
+            implementation(libs.ktor.client.darwin)
+        }
         commonMain.dependencies {
             implementation(compose.runtime)
             implementation(compose.foundation)
