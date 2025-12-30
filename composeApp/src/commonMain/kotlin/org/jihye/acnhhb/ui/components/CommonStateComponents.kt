c03774a5392c25516072ada6adb039557f34a8db
JihyeHan <jihyeh919@gmail.com>
2025-12-30 23:42:45 +0900

    공통 에러 컴포넌트 여백 추가

--- composeApp/src/commonMain/kotlin/org/jihye/acnhhb/ui/components/CommonStateComponents.kt
+++ composeApp/src/commonMain/kotlin/org/jihye/acnhhb/ui/components/CommonStateComponents.kt
@@ -2,12 +2,14 @@ package org.jihye.acnhhb.ui.components
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun LoadingContent(modifier: Modifier = Modifier) {
@@ -18,7 +20,10 @@ fun LoadingContent(modifier: Modifier = Modifier) {
 
 @Composable
 fun ErrorContent(message: String, modifier: Modifier = Modifier) {
-    Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+    Box(
+        modifier = modifier.fillMaxSize().padding(horizontal = 16.dp),
+        contentAlignment = Alignment.Center
+    ) {
         Text(text = message, color = MaterialTheme.colorScheme.error)
     }
 }
